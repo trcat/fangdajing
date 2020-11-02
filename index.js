67e8a59de1f4a0c83fa2b75f5bac1a805a1e72f6
DayDay <daydaybigking@sina.com>
2020-11-02 16:45:13 +0800

    优化选中 div 定位逻辑

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ class Fangdajin {
     this.canvas = null;
     this.ctx = null;
     this.areaRect = null;
-    this.areaRectX = 0;
-    this.areaReactY = 0;
+    this.areaRectWidth = 250;
+    this.areaRectHeight = 300;
 
     this.container.style.position = "relative";
   }
@@ -18,10 +18,8 @@ class Fangdajin {
   }
   generateCanvas() {
     const canvas = document.createElement("canvas");
-    canvas.width = window.getComputedStyle(this.container).width.split("px")[0];
-    canvas.height = window
-      .getComputedStyle(this.container)
-      .height.split("px")[0];
+    canvas.width = this.getElementWidth(this.container);
+    canvas.height = this.getElementHeight(this.container);
     this.canvas = canvas;
     this.ctx = this.canvas.getContext("2d");
     this.container.append(this.canvas);
@@ -31,6 +29,8 @@ class Fangdajin {
     areaRect.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     areaRect.style.position = "absolute";
     areaRect.style.display = "none";
+    areaRect.style.width = this.areaRectWidth + "px";
+    areaRect.style.height = this.areaRectHeight + "px";
     this.areaRect = areaRect;
     document.body.append(this.areaRect);
   }
@@ -46,22 +46,50 @@ class Fangdajin {
     document.addEventListener("mousemove", (e) => {
       const x = e.clientX;
       const y = e.clientY;
+      const containerWidth = this.getElementWidth(this.container);
+      const containerHeight = this.getElementHeight(this.container);
       const minTop = this.container.getBoundingClientRect().top;
-      const maxTop =
-        this.container.getBoundingClientRect().top +
-        Number(window.getComputedStyle(this.container).height.split("px")[0]);
+      const maxTop = minTop + containerHeight;
       const minLeft = this.container.getBoundingClientRect().left;
-      const maxLeft =
-        this.container.getBoundingClientRect().left +
-        Number(window.getComputedStyle(this.container).width.split("px")[0]);
+      const maxLeft = minLeft + containerWidth;
+
       if (x >= minLeft && x <= maxLeft && y >= minTop && y <= maxTop) {
+        let top = y - 150;
+        let left = x - 125;
+
+        if (x < minLeft + this.areaRectWidth / 2) {
+          left = minLeft;
+        } else if (x > maxLeft - this.areaRectWidth / 2) {
+          left = maxLeft - this.areaRectWidth;
+        }
+
+        if (y < minTop + this.areaRectHeight / 2) {
+          top = minTop;
+        } else if (y > maxTop - this.areaRectHeight / 2) {
+          top = maxTop - this.areaRectHeight;
+        }
+
         this.areaRect.style.display = "block";
-        this.areaRect.style.width = "250px";
-        this.areaRect.style.height = "300px";
-        this.areaRect.style.top = y - 150 + "px";
-        this.areaRect.style.left = x - 125 + "px";
+        this.areaRect.style.top = top + "px";
+        this.areaRect.style.left = left + "px";
       }
     });
+  }
+  /**
+   * 获取对应元素的宽度
+   * @param {HTMLElement} element
+   * @return {number}
+   */
+  getElementWidth(element) {
+    return Number(window.getComputedStyle(element).width.split("px")[0]);
+  }
+  /**
+   * 获取对应元素的高度
+   * @param {HTMLElement} element
+   * @return {number}
+   */
+  getElementHeight(element) {
+    return Number(window.getComputedStyle(element).height.split("px")[0]);
   }
 }
 
