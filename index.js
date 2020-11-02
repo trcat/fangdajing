6f3e4cb3ef9e66aed54b6d669e92240a877c4e03
DayDay <daydaybigking@sina.com>
2020-11-02 13:30:12 +0800

    修改写法，还是用 canvas 来实现放大镜

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const source = document.getElementById("source");
-source.style.position = "relative";
-source.style.overflow = "hidden";
-const area = document.createElement("div");
-area.style.position = "absolute";
-area.style.width = "100px";
-area.style.height = "100px";
-area.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-area.style.top = "-100px";
-area.style.left = "-100px";
-
-source.append(area);
-
-source.addEventListener("mousemove", (e) => {
-  const x = e.clientX - source.getBoundingClientRect().left;
-  const y = e.clientY;
-  let top, left;
-  if (x - 50 >= 0 && x + 50 <= 1121) {
-    left = x - 50;
-  } else if (x - 50 < 0) {
-    left = 0;
-  } else {
-    left = 1021;
+class Fangdajin {
+  constructor(options) {
+    this.container = document.querySelector(options.el);
+    this.img = null;
+    this.canvas = null;
+    this.ctx = null;
   }
-  if (y - 50 >= 0 && y + 50 <= 701) {
-    top = y - 50;
-  } else if (y - 50 < 0) {
-    top = 0;
-  } else {
-    top = 601;
+  init() {
+    this.generateCanvas();
   }
-  area.style.top = top + "px";
-  area.style.left = left + "px";
-});
+  generateCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.width = window.getComputedStyle(this.container).width.split("px")[0];
+    canvas.height = window
+      .getComputedStyle(this.container)
+      .height.split("px")[0];
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.container.append(this.canvas);
+    this.drawImage();
+  }
+  drawImage() {
+    const img = new Image();
+    img.src = this.container.getAttribute("data-img-src");
+    img.addEventListener("load", () => {
+      this.ctx.drawImage(this.img, 0, 0);
+    });
+    this.img = img;
+  }
+}
+
+new Fangdajin({
+  el: ".fangdajin",
+}).init();
