62525118b80b4fb17c907f384f80b35b89e7cc06
DayDay <daydaybigking@sina.com>
2020-11-02 16:10:10 +0800

    实现跟随鼠标的黑框

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ class Fangdajin {
     this.canvas = null;
     this.ctx = null;
     this.areaRect = null;
+    this.areaRectX = 0;
+    this.areaReactY = 0;
 
     this.container.style.position = "relative";
   }
@@ -12,6 +14,7 @@ class Fangdajin {
     this.generateCanvas();
     this.generateAreaRect();
     this.drawImage();
+    this.addEventListener();
   }
   generateCanvas() {
     const canvas = document.createElement("canvas");
@@ -23,6 +26,14 @@ class Fangdajin {
     this.ctx = this.canvas.getContext("2d");
     this.container.append(this.canvas);
   }
+  generateAreaRect() {
+    const areaRect = document.createElement("div");
+    areaRect.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    areaRect.style.position = "absolute";
+    areaRect.style.display = "none";
+    this.areaRect = areaRect;
+    document.body.append(this.areaRect);
+  }
   drawImage() {
     const img = new Image();
     img.src = this.container.getAttribute("data-img-src");
@@ -31,8 +42,26 @@ class Fangdajin {
     });
     this.img = img;
   }
-  generateAreaRect() {
-    
+  addEventListener() {
+    document.addEventListener("mousemove", (e) => {
+      const x = e.clientX;
+      const y = e.clientY;
+      const minTop = this.container.getBoundingClientRect().top;
+      const maxTop =
+        this.container.getBoundingClientRect().top +
+        Number(window.getComputedStyle(this.container).height.split("px")[0]);
+      const minLeft = this.container.getBoundingClientRect().left;
+      const maxLeft =
+        this.container.getBoundingClientRect().left +
+        Number(window.getComputedStyle(this.container).width.split("px")[0]);
+      if (x >= minLeft && x <= maxLeft && y >= minTop && y <= maxTop) {
+        this.areaRect.style.display = "block";
+        this.areaRect.style.width = "250px";
+        this.areaRect.style.height = "300px";
+        this.areaRect.style.top = y - 150 + "px";
+        this.areaRect.style.left = x - 125 + "px";
+      }
+    });
   }
 }
 
